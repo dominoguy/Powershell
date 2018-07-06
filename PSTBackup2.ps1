a4f325895880f43e16faf7a8f49081f078f30f0e
dominoguy <40834085+dominoguy@users.noreply.github.com>
2018-07-06 14:34:45 -0600

    friday

--- PSTBackup2.ps1
+++ PSTBackup2.ps1
@@ -2,3 +2,4 @@ New Backup PST
 Create an array of all the pst files in the source directory (Get-child)
 check to see if the pst is avaiable and if so put into another array
 do the robocopy of the available psts.
+more notes
