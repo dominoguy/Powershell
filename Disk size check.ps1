a748e5963111dd7b81e39ee6dde4716707dac260
dominoguy <40834085+dominoguy@users.noreply.github.com>
2018-07-13 17:02:58 -0600

    fridayafternoon

--- Disk size check.ps1
+++ Disk size check.ps1
@@ -4,12 +4,20 @@ $disk = Get-WmiObject win32_LogicalDisk -Filter "DeviceID='F:'" | Select-Object 
 
 $diskFreeSpace = [Math]::Round($disk.FreeSpace/1gb,2)
 $diskSize = [Math]::Round($disk.Size/1gb,2)
+$dataChange = [Math]::Round(8463589127/1gb,2)
+$dataSizeDiff = $diskFreeSpace-$dataChange
+
+if ($dataChange -lt $diskFreeSpace )
+{
+    Write-host 'There is room for this backup with ' $dataSizeDiff ' GB remaining.'
+}
+else {
+    Write-host 'There is not enough disk space.  The amount of diskspace required is ' $dataChange ' GB and only ' $diskFreeSpace ' GB is remaining.'
+}
+
+
 
 Write-host 'This is the amount of free space '$diskFreeSpace' GB'
 Write-host 'This is the size of the disk'$diskSize' GB'
-$dataChange = [Math]::Round(8463589127/1gb,2)
 write-host 'This is the amount of changed data '$dataChange ' GB'
-
-$dataSizeDiff = $diskFreeSpace-$dataChange
 write-host 'This is how much disk space will be left '$dataSizeDiff ' GB'
- 
