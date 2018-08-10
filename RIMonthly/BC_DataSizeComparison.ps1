26cb7a46b737a4e0d2418e875d828c66b9ecf301
dominoguy <40834085+dominoguy@users.noreply.github.com>
2018-08-10 11:52:43 -0600

    friday

--- RIMonthly/BC_DataSizeComparison.ps1
+++ RIMonthly/BC_DataSizeComparison.ps1
@@ -83,10 +83,10 @@ If ($prevMonth -le 10)
 }
 
 
-write-Log "This is the current month $currentMonth"
-write-Log "This is the previous month $prevMonth"
-write-Log "This is the current year $currentYear"
-write-Log "This is the previous year $prevYear"
+#write-Log "This is the current month $currentMonth"
+#write-Log "This is the previous month $prevMonth"
+#write-Log "This is the current year $currentYear"
+#write-Log "This is the previous year $prevYear"
 
 #Create the comparison reports
 $runPath = $PSScriptRoot
@@ -94,7 +94,6 @@ $bcApp="C:\Program Files\Beyond Compare 4\BCompare.exe"
 $bcScptSnap = "$runPath\BCSnap.txt"
 $bcCompareSize = "@$runPath\Comparesize.txt"
 
-Write-Host "This is the bcss compare file $bcCompareSize"
 
 $list = Import-Csv $ServerList
 
@@ -116,8 +115,8 @@ foreach ($row in $list)
     #$dirServer = $Path[3]
 
     #write-Log "This is the drive $drive"
-    write-Log "This is folder1 $folder1"
-    write-Log "This is folder2 $folder2"
+    #write-Log "This is folder1 $folder1"
+    #write-Log "This is folder2 $folder2"
     #write-Log "This is the root folder $dirFolder"
     #write-Log "This is the client acronym $dirClient"
     #write-Log "This is the server $dirServer"
@@ -125,19 +124,68 @@ foreach ($row in $list)
     $bcssFileDir = "$drive\$folder1\$folder2\$dirBaseline\$dirClient\$dirServer"
     $bcssFileName = "${currentMonth}_$currentYear"
     $bcssFile = "$bcssFileDir\${currentMonth}_$currentYear.bcss"
+    $bcssReport = "$bcssFileDir\${currentMonth}_$currentYear"
   
-    $bcssFile = "$drive\$dirBaseline\$dirClient\$dirServer\${currentMonth}_$currentYear.bcss"
+    $bcssFile = "$drive\$folder1\$folder2\$dirBaseline\$dirClient\$dirServer\${currentMonth}_$currentYear.bcss"
 
-    write-Log "This is the BCSS file location $bcssFile"
+    #write-Log "This is the BCSS file location $bcssFile"
 
-    write-Log "this is the bcss file directory $bcssFileDir"
-    write-Log "This is the BCSS report file name $bcssFileName"
-    write-Log "This is the beyond compare  $bcApp"
+    #write-Log "this is the bcss file directory $bcssFileDir"
+    #write-Log "This is the BCSS report file name $bcssFileName"
+    #write-Log "This is the beyond compare  $bcApp"
 
-    write-Log "Running the Beyond Compare"
+    write-Log "Running the comparison for $dirserver "
 
     Write-host $bcApp $bcCompareSize $ClientDir $bcssFile $bcssFileDIr $bcssFilename
     & $bcApp $bcCompareSize $ClientDir $bcssFile $bcssFileDIr $bcssFilename
+
+    [xml]$XmlDocument = Get-content -Path "${bcssReport}_Report.xml"
+    #write-Log "This is the report path ${bcssReport}_Report.xml"
+#Check for free space on a drive
+    [int]$driveLimit = '100000000'
+    [int]$driveLimitMB = [Math]::Round('100000000'/1mb,2)
+
+    $disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='F:'"
+    Select-Object Size,FreeSapce
+
+    $disksize = $disk.size
+    $diskFree = $disk.Freespace
+    $disksizeMB = [Math]::Round($disk.size/1mb,2)
+    $diskFreeMB = [Math]::Round($disk.Freespace/1mb,2)
+
+    write-Log "The drive limit is $driveLimitMB MB"
+    write-Log "Total disk size is $disksizeMB MB"
+    Write-Log "Total free disk space is $diskFreeMB MB"
+
+    $LTfolderdata = $XmlDocument.bcreport.foldercomp.foldercomp.lt.name
+    $LTfolderdatasize = $XmlDocument.bcreport.foldercomp.foldercomp.lt.size
+    Write-Log "This is the size of the left folder in comp $LTfolderdatasize"
+    [int]$RawLTSize = $LTfolderdatasize -replace '[,]',''
+
+    $RTfolderdata = $XmlDocument.bcreport.foldercomp.foldercomp.rt.name
+    $RTfolderdatasize = $XmlDocument.bcreport.foldercomp.foldercomp.rt.size
+    Write-Log "This is the size of the right folder in comp $RTfolderdatasize"
+    [int]$RawRTSize = $RTfolderdatasize -replace '[,]',''
+
+
+    $RawBackupSize = $LTfolderdatasize-$RTfolderdatasize
+    #[int]$RawBackupSize = '48273683'
+    $BackupSize = [Math]::Round($RawBackupSize/1mb,2)
+
+    Write-Log "This is the size of the backup: $BackupSize MB"
+    $SpaceRemain = $diskFree - $RawBackupSize
+    $SpaceRemainMB = [Math]::Round($SpaceRemain/1mb,2)
+
+    If ($SpaceRemain -gt $driveLimit)
+    {
+        Write-Log "There is room for the backup with $SpaceRemainMB Mb remaining"
+    }
+    else
+    {
+        Write-Log "Backup is too big. you need $SpaceRemain MB more"
+    }
+
+
 
 }
 
