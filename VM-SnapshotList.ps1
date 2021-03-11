b00385c476335f1483992e6e712437013dc93767
dominoguy <40834085+dominoguy@users.noreply.github.com>
2021-03-10 17:01:59 -0700

    10march2021

--- VM-SnapshotList.ps1
+++ VM-SnapshotList.ps1
@@ -1,5 +1,15 @@
 #VM-SnapshotList
 
+
+function ConvertToGB
+{
+    Param(
+        [Int]$Size
+    )
+    [Math]::Round($Size/1gb,2)
+}
+
+
 function Write-Log
 {
     Param(
@@ -54,13 +64,32 @@ foreach ($row in $Serverlist)
     
     $servername = $row.name
     Write-log $servername
+    #add line to results for hvs, check drive on c and d, total disk and free space
+    #
+     #Connect to Server
+     $cdisk = Invoke-Command -ComputerName $servername -ScriptBlock{Get-WmiObject win32_LogicalDisk -Filter "DeviceID='C:'" | Select-Object Size,FreeSpace,DeviceID}
+     $cdiskFreeSpace = [Math]::Round($cdisk.FreeSpace/1gb,2)
+     $cdiskSize = [Math]::Round($cdisk.Size/1gb,2)
+     $cdiskName = $cdisk.DeviceID
+     #$cdiskFreeSpace = ConvertToGB $disk.FreeSpace
+     #$cdiskSize - ConvertToGB $disk.Size
+ 
+     $Results = [PSCustomObject]@{
+         ServerName = "$Servername"
+         DriveName = "$cdiskName"
+         FreeSpace = "$cdiskFreeSpace"
+         DiskSize = "$cdiskSize"
+     } 
+     $Results | Export-Csv -Path $ResultsCSV -Append -NoTypeInformation -Force
+
+
     $vms = Invoke-Command -Computername $servername -ScriptBlock{get-vm |select-object -Expandproperty Name}
 
     foreach ($vm in $vms)
     {
         write-log "    $vm"
         Write-Host $vm
-
+        #add line in results for each vm, vm status, 
         $Snapshots = Invoke-Command -Computername $servername -ScriptBlock{param($var1) get-vm $var1 | Get-VMSnapshot} -ArgumentList $vm
         foreach ($item in $Snapshots) 
         {
