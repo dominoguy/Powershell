ec46f39ce1db0bd52ad192d4242b570151e21f2f
dominoguy <40834085+dominoguy@users.noreply.github.com>
2022-11-22 12:03:31 -0700

    22NOV2022

--- RIBackups/Mirroring/BackupServerClientMirror.ps1
+++ RIBackups/Mirroring/BackupServerClientMirror.ps1
@@ -1,8 +1,11 @@
 #BackupServerClientMirror
 
 #This script uses Beyond Compare to mirror the client data on the backup server with the client server, keeping newer files on the backup server at specified date.
-#Requirements: A Beyond Compare Snapshot of the client server, note the date it is started as the snapshot may take days to complete.
-#Note: Any files, created on or newer than the start date of the snapshot or the last date of a completed daily backup whichever is the oldest, are to be kept.
+#This script can be run after:
+#a) The yearly backup of the client is done
+#b) The BCSS of the client server is done
+#Requirements: A Beyond Compare Snapshot of the client server, note the date as it is required to keep newer files on the backup server
+
 
 
 
@@ -16,7 +19,7 @@ function Write-Log
     Add-Content $Logfile -value "$Time $logstring"
 }
 
-$LogLocation = "F:\Data\Scripts\Powershell\Backups\Mirroring\ClientMirror.log"
+$LogLocation = "F:\Data\Scripts\Powershell\RIBackups\Mirroring\ClientMirror.log"
 $logFile = $LogLocation
 
 if ( $logFileExists -eq $True)
@@ -29,23 +32,36 @@ else
     New-Item -ItemType File -Force -Path $logFile
 }
 
-#Add server list and csv file pull
-
-
 Write-Log "Start Client Mirror"
 
+#Add server list and csv file pull
+$ClientsList = "F:\Data\Scripts\Powershell\RIMonthly\ClientsList.csv"
+$Clients = Import-CSV -Path $ClientsList | select-object -Property Client,ServerName,BackupDir,TargetDir,bcssDir,bcssFile,CutOffDate
+
+
 $bcApp = "C:\Program Files\Beyond Compare 4\BCompare.exe"
-$bcClientSnapshot = "F:\Data\Scripts\Powershell\Backups\Mirroring\TEST2_2022-11-16.bcss"
-#$bcClientSnapshot = ""
-$bcScptMirror = "F:\Data\Scripts\Powershell\Backups\Mirroring\BCMirrorScript.txt"
-#$bcMirror = ""
+$bcScptMirror = "F:\Data\Scripts\Powershell\RIBackups\Mirroring\BCMirrorScript.txt"
+#$bcScptMirror = "D:\Data\Scripts\RIBackup\ClientMirror\BCMirrorScript.txt"
 
-$ClientDir = "F:\TEST1"
-$bcLog = "F:\Data\Scripts\Powershell\Backups\Mirroring\Mirror.Log"
-$CutOffDate = "11/16/2022"
 
-$argsBCSS = "@$bcScptMirror /closescript $ClientDir $bcClientSnapshot $bcLog $CutOffDate"
-Start-Process -FilePath $bcApp -ArgumentList $argsBCSS -wait
-Write-Log "Completed mirroring of client data on backup server. See Mirror.log for details"
+ForEach ($client in $Clients)
+{
+    $client = $client.Client
+    $serverName = $client.ServerName
+    $backupDir = $client.ClientDir
+    $TargetDir = $client.TargetDir
+    $BCSSDir = $client.BCSSDir
+    $bcssFile = $client.bcssFile
+    $CutOffDate = $client.$CutOffDate
+    $bcClientSnapshot = "$BCSSDir\$bcssFile"
+    $ClientDir = "$backupDir\$client\$servername\$TargetDir"
 
+    $bcLog = "F:\Data\Scripts\Powershell\RIBackups\$serverName-Mirror.Log"
+    #$bcLog = "D:\Data\Scripts\RIBackup\ClientMirror\$serverName-Mirror.Log"
+
+    Write-Log "Starting Mirroring of $servername"
+    $argsBCSS = "@$bcScptMirror /closescript $ClientDir $bcClientSnapshot $bcLog $CutOffDate"
+    Start-Process -FilePath $bcApp -ArgumentList $argsBCSS -wait
+    Write-Log "Completed mirroring of $servername. See $servername-Mirror.log for details"
+}
 Write-Log "Finished Client Mirror"
